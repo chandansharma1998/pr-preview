aec23cf3e7bc406a6a65153951966b353d92f9a5
chandansharma1998 <sharmachandan487@gmail.com>
2025-08-26 18:28:53 +0530

    main changes

--- app/main.go
+++ app/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from PR Preview!!! PR=%s", os.Getenv("PR_NUMBER"))
+	fmt.Fprintf(w, "Hello from PR Preview")
 }
 
 func main() {
